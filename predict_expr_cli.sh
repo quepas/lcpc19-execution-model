509121c94e3e4bf441873bcab030258945443904
quepas <kiepas.patryk@gmail.com>
2020-02-05 23:46:05 +0100

    Predict execution for expressions with whitespaces as well

--- predict_expr_cli.sh
+++ predict_expr_cli.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp execution_model/lib/execution_model-1.0.jar hum.runner.ExecutionPredictor $1
+java -cp execution_model/lib/execution_model-1.0.jar hum.runner.ExecutionPredictor "$@"
